5fa1c2e213747f38afd0a554399c8489bbfa3c73
tnatsume@vitalify.jp <tnatsume@vitalify.jp>
2019-06-19 16:55:34 +0900

    Initial commit

---
+++ polls/urls.py
@@ -0,0 +1,23 @@
+
+from django.conf.urls import url
+
+from . import views
+
+app_name = 'polls'
+urlpatterns = [
+    # ex: /polls/
+    url(r'^$', views.IndexView.as_view(),name='index'),
+    # url(r'^$', views.IndexView.as_view(), name='index'),
+
+    # ex: /polls/5¥
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view() , name='detail'),
+    # url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+
+    # ex: polls/5/results/
+    url(r'^(?P<pk>[0-9]+)/results/$',views.ResultView.as_view(),name='results'),
+    
+    # ex: polls/5/vote/
+    url(r'^(?P<question_id>[0-9]+)/vote/$',views.vote,name='vote'),
+
+
+]
